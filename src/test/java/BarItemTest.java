ee7a96acc35e85002da8d2cfcb6d383129ebcad1
Miguel <miguelmoutela@hotmail.com>
2017-11-21 14:49:40 +0000

    BarItem - all tests passing

---
+++ src/test/java/BarItemTest.java
@@ -0,0 +1,26 @@
+import Menu.BarItem;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BarItemTest {
+    BarItem barItem;
+
+    @Before
+    public void before() {
+        barItem = new BarItem("thatDrink", 5,6);
+    }
+
+    @Test
+    public void canGetNameToLowerCase() {
+        assertEquals("thatdrink", barItem.getName());
+    }
+
+    @Test
+    public void canGetPrice() {
+        assertEquals(5, barItem.getPrice(), 0.01);
+    }
+    @Test
+    public void canGetQuantity() {assertEquals(6,barItem.getQuantity());}
+}
